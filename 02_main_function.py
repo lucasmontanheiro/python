6639c919b67f8f74585bfe6c1ffe3b6e25bfeecf
Lucas Montanheiro <lucasmontanheiro@gmail.com>
2022-09-09 16:57:00 -0700

    adding python main file

---
+++ 02_main_function.py
@@ -0,0 +1,11 @@
+# Explain why python files often include the __main__ function
+
+# The main function is implemented to make sure that this library uses 
+# the prefix library_name.function in future uses
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
+
